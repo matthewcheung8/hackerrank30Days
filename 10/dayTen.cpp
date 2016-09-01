c47f0a85790c49df16b2f4d88afd57dcdc5d18e8
Matthew Cheung <matthewcheung@umail.ucsb.edu>
2016-09-01 14:53:34 -0700

    Finish dayTen

---
+++ 10/dayTen.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+	int n;
+	cin >> n;
+	int count = 0;
+	while (n) {
+		n = (n & (n << 1));
+		count++;
+	}
+	cout << count;
+	return 0;
+}
